368fd2550fb729a7549bbc7a538160caf26f62d7
stu2301321017 <stu2301321017@uni-plovdiv.bg>
2026-02-19 18:07:39 +0200

    generalize toy datasets structure, switch to mp3 format

--- assemble.py
+++ assemble.py
@@ -1,20 +1,21 @@
 import os
 from pydub import AudioSegment
 
-dataset = "/00 Dummy"
+DATASET = "/01 Dummy-Pc"
+NAMES_COUNT = 3
 
-dataset_dir = "Datasets/" + dataset
-output_dir = f"Datasets/{dataset}/output"
+dataset_dir = "Datasets/" + DATASET
+output_dir = f"Datasets/{DATASET}/output"
 os.makedirs(output_dir, exist_ok=True)
 
-prefix = AudioSegment.from_file(os.path.join(dataset_dir, "ne sam siguren no mislq che.m4a"))
-suffix = AudioSegment.from_file(os.path.join(dataset_dir, "govorq.m4a"))
+prefix = AudioSegment.from_file(os.path.join(dataset_dir, "start.mp3"))
+suffix = AudioSegment.from_file(os.path.join(dataset_dir, "end.mp3"))
 
-names = ["dancho", "kondio", "mitko", "petar", "rumqna"]
+names = ["name-00" + str(i+1) for i in range(NAMES_COUNT)]
 
 for name in names:
-    middle = AudioSegment.from_file(os.path.join(dataset_dir, f"{name}.m4a"))
+    middle = AudioSegment.from_file(os.path.join(dataset_dir, f"{name}.mp3"))
     combined = prefix + middle + suffix
-    out_path = os.path.join(output_dir, f"{name}_combined.m4a")
-    combined.export(out_path, format="ipod")
+    out_path = os.path.join(output_dir, f"{name}_combined.mp3")
+    combined.export(out_path, format="mp3")
     print(f"Saved: {out_path}")
